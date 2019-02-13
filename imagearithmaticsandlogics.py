cd572009499ab83692ce093d9023700bf240d08b
Abhijeet Bhattacharya <31439714+Abhijeet399@users.noreply.github.com>
2019-02-14 00:58:33 +0530

    Operations_on_image

---
+++ imagearithmaticsandlogics.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+img1=cv2.imread('3D-Matplotlib.png')
+img2=cv2.imread('mainlogo.png')
+
+#add = img1+img2
+#add=cv2.add(img1,img2)     cv2.add adds all the pixel values
+#weighted=cv2.addWeighted(img1,0.6,img2,0.4,0)     [   0=gamma value]
+
+# I want to put logo on top-left corner, So I create a ROI
+rows,cols,channels = img2.shape
+roi = img1[0:rows,0:cols]
+
+#print(roi)
+
+img2gray=cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
+ret,mask=cv2.threshold(img2gray,220,255,cv2.THRESH_BINARY_INV)
+#ROLL OF BINARY_INV    if a pixel vlaue is below 220 its converted to white
+                       #if above than 220 then to black
+#print(mask)
+mask_inv=cv2.bitwise_not(mask) 
+
+# Now black-out the area of logo in ROI
+img1_bg=cv2.bitwise_and(roi,roi,mask=mask_inv)   
+# Take only region of logo from logo image.
+img2_fg=cv2.bitwise_and(img2,img2,mask=mask)
+
+# Put logo in ROI and modify the main image
+dst=cv2.add(img1_bg,img2_fg)
+img1[0:rows,0:cols] = dst
+
+#cv2.imshow('res',img1) 
+cv2.imshow('img1_bg',img1_bg) 
+cv2.imshow('img2_fg',img2_fg) 
+cv2.imshow('dst',dst) 
+#cv2.imshow('mask_inv',mask_inv) 
+cv2.imshow('img2gray',img2gray) 
+cv2.imshow('mask',mask) 
+
+#cv2.imshow('mask',mask)
+#cv2.imshow('img2',img2)
+
+
+#cv2.imshow('add',weighted)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
